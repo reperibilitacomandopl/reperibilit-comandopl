241ca9fc78772d7c8799e004c84a86b6bfaebf27
Mario <mariobros1987@gmail.com>
2026-05-02 18:56:00 +0200

    Fix: CSP whitelist cartocdn.com for map tiles

--- src/middleware.ts
+++ src/middleware.ts
@@ -139,9 +139,9 @@ function addSecurityHeaders(response: NextResponse, isPublic: boolean = false): 
     default-src 'self';
     script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com https://*.tile.openstreetmap.org;
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com;
-    img-src 'self' data: https://*.tile.openstreetmap.org https://unpkg.com https://*.googleusercontent.com https://raw.githubusercontent.com;
+    img-src 'self' data: blob: https://*.tile.openstreetmap.org https://*.basemaps.cartocdn.com https://unpkg.com https://*.googleusercontent.com https://raw.githubusercontent.com;
     font-src 'self' https://fonts.gstatic.com;
-    connect-src 'self' https://api.telegram.org https://*.tile.openstreetmap.org https://unpkg.com https://assets.mixkit.co;
+    connect-src 'self' https://api.telegram.org https://*.tile.openstreetmap.org https://*.basemaps.cartocdn.com https://unpkg.com https://assets.mixkit.co;
     media-src 'self' https://assets.mixkit.co;
     frame-ancestors 'none';
     form-action 'self';
